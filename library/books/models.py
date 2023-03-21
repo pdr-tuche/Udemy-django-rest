8d8bd32d446c7833a434c3285ed75b55313bcef2
Pedro <contato.nevespedro@gmail.com>
2023-03-20 23:25:30 -0300

    criado model de book'

--- library/books/models.py
+++ library/books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
+
+class Books (models.Model):
+    idBook = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title =  models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    releaseYear = models.IntegerField()
+    state = models.CharField(max_length=50),
+    pages = models.IntegerField(),
+    publishingCompany = models.CharField(max_length=255)
+    createAt = models.DateField(auto_now_add=True)
+    
